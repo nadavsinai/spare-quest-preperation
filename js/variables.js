c72efad1d53150f80dc9e8e622cb9acc97f08756
Nadav Sinai <nadav@algotec.co.il>
2020-01-03 00:03:05 +0200

    variables 1

---
+++ js/variables.js
@@ -0,0 +1,33 @@
+var space;
+let force = 1;
+const pie=3.14;
+
+function getThrust(weight){
+  space = force *weight * pie;
+  console.log(result);
+  var result = space+'KG';
+  return result;
+}
+
+function loopOnPlanets(){
+  for (var starIndex=0; starIndex < stars.length;starIndex++){
+  }
+  console.log(starIndex);
+}
+
+
+const forceNeeded = getThrust(2);
+console.log(forceNeeded);
+
+var stars = ['earth',
+'jupiter',
+'mars',
+'mercury',
+'moon',
+'neptune',
+'saturn',
+'uranus',
+'venus'];
+
+loopOnPlanets();
+
