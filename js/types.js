bbd2e290e165a6d3a8794586aebc132c5c27946b
Nadav Sinai <nadav@algotec.co.il>
2020-01-03 00:03:24 +0200

    types 1

---
+++ js/types.js
@@ -0,0 +1,58 @@
+// js has types
+
+console.log('something',typeof 'something'); //string
+console.log(123,typeof 123) //number
+console.log(12.132143,typeof 12.132143); // number - floats and ints  are the same
+console.log(12n,typeof 12n); // bigint -  we now have bigint
+console.log(true,typeof true); // boolean
+console.log(Symbol('x'),typeof Symbol('x')); //symbol
+console.log({},typeof {}); // object
+// but that's where type logic stops.
+//array is actually a kind of object
+//console.log(['string',121,[{}]],typeof ['string',121,[{}]]); // and you can mix values inside it
+
+// we have constructor based differentiation of objects
+
+function Toy(type){
+  this.type = type;
+  return this;
+}
+Toy.prototype = {
+  play(){
+    return 'playing';
+  }
+}
+
+var train = new Toy('train');
+var train1 = new Toy();
+train.play();  //?
+
+
+const called = Toy('other');
+called.type; //?
+console.log(train instanceof Toy);
+
+// class Pet{}
+// var cat = new Pet();
+// const calledClassasFunction = Pet(); //error : Class constructor Pet cannot be invoked without 'new'
+
+// equality has 2 levels
+// value equality
+// type equality
+console.log(3 == '3');//true !! cause of bugs
+console.log(3 === '3'); //false
+console.log(train.constructor === train1.constructor) // .?
+// there are some weird bits also for ====
+NaN === NaN;     //?       // false
+0 === -0; // ?
+
+var x = [ 1, 2, 3 ];
+
+// assignment is by reference-copy, so
+// y references the *same* array as x,
+// not another copy of it.
+var y = x;
+
+y === x;              // true
+y === [ 1, 2, 3 ];    // false
+x === [ 1, 2, 3 ];    // false
