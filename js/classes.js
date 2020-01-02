1ae26dcda2d8744b4ba7fcc7b4f1f1b308dc01f7
Nadav Sinai <nadav@algotec.co.il>
2020-01-03 00:03:48 +0200

    classes 1

---
+++ js/classes.js
@@ -0,0 +1,71 @@
+class FuelSupply {
+  tankCapacity = 0;
+  energyPerKg = 0;
+  flow = 0;
+  price = 0;
+  complexity = 0;
+
+  pump() {
+    console.log('starting to pump!');
+  }
+}
+
+class YeOldePetrolFuelSupply extends FuelSupply {
+  energyPerKg = 50;
+  flow = 10;
+  price = 2000;
+  complexity = 20;
+
+  constructor(tankCapacity = 40) { //default value
+    super();
+    this.tankCapacity = tankCapacity; // note we can't use this before super();
+  }
+}
+
+class Engine {
+  #started = false; // field
+  fuelSupply;
+
+  stop() {
+    this.#started = false
+  }
+
+  constructor(fuelSupply) { //optional
+    this.fuelSupply = fuelSupply;
+  }
+
+  start() { // method
+    this.fuelSupply.pump();//?
+    console.log('started engine!');
+    this.#started = true;
+  }
+}
+
+class JetEngine extends Engine {
+  airDuckt = { open: false, flow: 0 };
+
+  constructor(pump) {
+    super(pump);
+
+  }
+
+  start() {
+    this.airDuckt.open = true;
+    this.airDuckt.flow = 1;
+    try {
+      super.start();
+    } catch (e) {
+      console.log(e);
+      this.airDuckt.flow = 0;
+      this.airDuckt.open = false
+    }
+  }
+
+  stop() {
+
+  }
+
+}
+
+const myJet = new JetEngine(new YeOldePetrolFuelSupply());
+myJet.start(); //?
